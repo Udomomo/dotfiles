a906f1a11902d5db137baf89a86283d646c48adc
Udomomo <udomomo@gmail.com>
2023-03-10 23:50:01 +0900

    Add brew prefix

--- bin/install.sh
+++ bin/install.sh
@@ -35,6 +35,7 @@ homebrew() {
   which brew >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
   echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
   eval "$(/opt/homebrew/bin/brew shellenv)"
+  eval "$(brew --prefix)"
 }
 
 deploy() {
