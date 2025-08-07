a8733009484579d32094408b51514088405af006
Udomomo <udomomo@gmail.com>
2025-08-08 00:51:36 +0900

    add comment to apply_patch

--- bin/apply_patch.sh
+++ bin/apply_patch.sh
@@ -20,10 +20,15 @@ apply_patch() {
   cd "$(dirname "$0")"
   cd ../
 
+  # 調査用にpatchファイルは残したいが、同名で作ると適用済みとみなされるのでsuffixをつける
   PATCH_FILE_SUFFIX=$(date +%s)
 
+  # patchコマンドはパスを含めた名前が短い方に当てようとするので、今いるディレクトリをPWDではなく相対パスで指定している
   diff --exclude=.git -urN . /tmp/dotfiles-master > /tmp/dotfiles_diff_${PATCH_FILE_SUFFIX}.patch || echo "diff has detected"
-  patch -u -p3 -d . < /tmp/dotfiles_diff_${PATCH_FILE_SUFFIX}.patch 
+  patch -u -p0 -d . < /tmp/dotfiles_diff_${PATCH_FILE_SUFFIX}.patch
+
+  # レポジトリから削除したファイルが残ってしまうことがあるため、ダウンロードしたファイルは削除しておく
+  rm -rf /tmp/master.tar.gz /tmp/dotfiles-master
 }
 
 link() {
