8c0254b8ed6e6bffce821edbebabec63c0b1a834
Udomomo <udomomo@gmail.com>
2026-03-17 14:12:44 +0900

    update wezterm leader key

--- wezterm.lua
+++ wezterm.lua
@@ -72,7 +72,7 @@ wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_wid
   end
 end)
 
-config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 2000 }
+config.leader = { key = "q", mods = "CTRL", timeout_milliseconds = 2000 }
 
 local color_scheme = 'kanagawa (Gogh)'
 config.color_scheme = color_scheme
